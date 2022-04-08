f67400f7bbcae62476304cbd7c32e07203a40326
yosephhbt <josephhbt@gmail.com>
2022-04-08 11:50:25 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all possible combination of single-digit
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+
+		if (i != 9)
+		{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
