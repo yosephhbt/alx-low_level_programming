6e045871eae91fdfba81222bde6f74023d85d49c
yosephhbt <josephhbt@gmail.com>
2022-04-08 12:43:12 -0700

    insomnia

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 
 /**
- *  main - Prints the alphabetic in lower and upper case
+ *  main - entry point
+ * Description: Prints the alphabetic in lower and upper case
  * 
- * Return: Always (Success)
+ * Return: 0
  */
 
 int main(void)
 {
 	char c;
-
+	
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
-
 	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
 	}
-
 	putchar('\n');
-
 	return (0);
 }
