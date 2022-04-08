136e2c95a6537c6187ed91067296d448bf4da97e
yosephhbt <josephhbt@gmail.com>
2022-04-08 11:13:29 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Printssingle digit numbers
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
+
