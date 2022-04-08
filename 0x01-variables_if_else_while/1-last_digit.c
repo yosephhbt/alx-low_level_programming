3305454420d3fe030b5769f9884c4f82907ffe73
yosephhbt <josephhbt@gmail.com>
2022-04-08 12:30:25 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - Prints a text according number
- * 
- * Return: Always (Success)
+ * main - entry point
+ *
+ * Description: Prints a text according number
+ * Return: 0
  */
 int main(void)
 {
 	int n, lastd;
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
