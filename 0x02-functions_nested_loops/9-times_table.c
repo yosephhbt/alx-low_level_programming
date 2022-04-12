4a963f66e61766c266a1423f5f13ab8624560d9a
yosephhbt <josephhbt@gmail.com>
2022-04-12 10:44:30 -0700

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,14 +6,30 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int rone, cone, d;
 
-	for (i = 0; i < 10; i++)
+	for (rone = 0; rone <= 9; rone++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
 		{
-			_putchar((i * j) + '0');
-			_putchar(',');
+			d = (rone * cone);
+			if ((d / 10) > 0)
+			{
+				_puchr((d/ / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
