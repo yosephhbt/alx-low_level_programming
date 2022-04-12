4e762b97a2f2fddb169199aa42cf88491b168a1a
yosephhbt <josephhbt@gmail.com>
2022-04-12 04:02:51 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while(*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
