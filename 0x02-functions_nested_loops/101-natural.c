7f11a12d5f2ac8698732c17a3e4994617913de47
yosephhbt <josephhbt@gmail.com>
2022-04-13 04:18:06 -0700

    sum of all multiples of 3 & 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: int
+ */
+int main(void)
+{
+	int n = 0;
+	int sum = 0;
+
+	while (n < 1024)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+		{
+		sum += n;
+		n++;
+	}
+	return (sum);
+}
+
+
+			
