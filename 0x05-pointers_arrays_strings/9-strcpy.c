c26f14966869e389433d2410fa0541016bec684a
yosephhbt <josephhbt@gmail.com>
2022-04-19 03:50:07 -0700

    cpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strcpy - copies string to buffer both arguments
+ * @dest:input 1
+ * @src:input 2
+ * Return: pointr to the buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
+}
