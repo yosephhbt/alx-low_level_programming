0029c07e1e445e571c394e460286aef5bb2b10b8
yosephhbt <josephhbt@gmail.com>
2022-04-20 02:40:03 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcat - a function that concatenates
+ * @dest: input 1
+ * @src: input 2
+ * Return: a pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
