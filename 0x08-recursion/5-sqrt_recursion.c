bbd8d865aeeae9d47e4fc9e30293366618b0d204
yosephhbt <josephhbt@gmail.com>
2022-04-27 03:24:44 -0700

    commite

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * sqrt2 - evalutes 1 to n
+ * _sqrt_recursion - evalutes 1 to n
  * @a: input
  * @b: number that iterates from 1 to n
  * Return: on success 1
  */
-int sqrt2(int a, int b)
+int _sqrt_recursion(int a, int b)
 {
 	if (b * b == a)
 		return (b);
 	else if (b * b > a)
 		return (-1);
-	return (sqrt2(a, b + 1));
+	return (_sqrt_recursion(a, b + 1));
 }
