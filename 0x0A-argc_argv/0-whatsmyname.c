7188b0d7271603171bbaf6cd64ea6c51fcc9ae8d
yosephhbt <josephhbt@gmail.com>
2022-04-29 07:28:03 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the number of arguments passed into main
+ * @argc: number of command line arguments
+ * @argv: array name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+README.md for the project 0X0A. C - argc, argv
