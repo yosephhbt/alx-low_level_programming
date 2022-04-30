8508c897198e7962b48318898e7bf1ba3b8b7e6d
yosephhbt <josephhbt@gmail.com>
2022-04-30 09:28:40 -0700

    coin

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main,h"
+#include <ctype.h>
+#include <stdbool.h>
+/**
+ * coinConverter - Helper function that does all the mathematics
+ * @i:variable
+ * Return: number of coins
+ */
+int coinConverter(int i)
+{
+	int count = 0;
+
+	while (i != 0)
+	{
+		if (i % 10 == 9 || i % 10 == 7)
+			i -= 2;
+		else if (i % 25 == 0)
+			i -= 25;
+		else if (i % 10 == 0)
+			i -= 10;
+		else if (i % 5 == 0)
+			i -= 5;
+		else if (i % 2 == 0)
+		{
+			if (i% 10 == 6)
+				i -= 1;
+			else
+				i -=2;
+		}
+		else
+			i -= 1;
+		count++;
+	}
+	return (count);
+}
+
 /**
  * main - prints the minimum number of coins to make change for an amount of money
  * @argc: argument count
@@ -9,40 +44,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b, cent = 0;
-	int change[5] = {25, 10, 5, 2, 1};
+	int i, coin;
 
+	coin = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	a = atoi(argv[1]);
-	if (a < 0)
-	{
+	i = atoi(argv[1]);
+	if (i < 0)
 		printf("0\n");
-		return (0);
-	}
 	else
 	{
-		for (b = 0; b < 5; b++)
-		{
-			if (a >= change[b])
-			{
-				a -= change[b];
-				cent += 1;
-				if (a >= change[b])
-				{
-					b--;
-				}
-				else if (a == 0)
-				{
-					break;
-				}
-			}
-		}
-		printf("%d\n", cent);
-		return (0);
+		coin = coinConverter(i);
+		printf("%d\n", coin);
 	}
+	return (0);
 }
-					
