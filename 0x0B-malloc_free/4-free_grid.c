a459de741e9e2492be09f468d8eb137f51bffaf6
yosephhbt <josephhbt@gmail.com>
2022-05-02 01:27:24 -0700

    2 dimensional array

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,8 +8,9 @@
 void free_grid(int **grid, int height)
 {
 	int a;
-	
-	if((grid == NULL) || (height == 0))
+
+
+	if ((grid == NULL) || (height == 0))
 		return;
 	for (a = 0; a < height; a++)
 		free(grid[a]);
