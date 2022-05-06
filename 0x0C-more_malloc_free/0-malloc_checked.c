9239f1ea26864490af59231244452798a3c39adb
yosephhbt <josephhbt@gmail.com>
2022-05-06 06:04:51 -0700

    first commmit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: amount of bytes
+ * Return: pointer to our new allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
