4b3dbf161dc606bad30c6b520471996e338bb431
yosephhbt <josephhbt@gmail.com>
2022-05-09 07:09:12 -0700

    print

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * main - program that prints out the name of the file being compiled
+ * @void: Noting
+ * Return: success
+ */
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
