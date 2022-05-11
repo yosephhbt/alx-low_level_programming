f69a6649bf2e67f6fc2b1a1ac240a6b7ab5145f1
yosephhbt <josephhbt@gmail.com>
2022-05-11 11:47:37 -0700

    poit_funct

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,7 +3,6 @@
 #include <stddef.h>
 
 int _putchar(char c);
-typedef __SIZE_TYPE__size_t;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
