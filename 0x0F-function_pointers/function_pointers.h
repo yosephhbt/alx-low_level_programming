342e31aba30a817a0892d6de92899dd613ba1c6c
yosephhbt <josephhbt@gmail.com>
2022-05-11 07:07:37 -0700

    func_pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
+int _putchar(char c);
 typedef __SIZE_TYPE__size_t;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size, size_t size, void (*action)(int));
