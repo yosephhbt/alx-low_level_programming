5408b5e3478a954b6fa608e88a595f03a1d64881
yosephhbt <josephhbt@gmail.com>
2022-05-11 07:01:02 -0700

    func_pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 
-	if (action != 0 && size >= 1 && array != 0)
-	{
-		for (i = 0; i < size; i++)
-			action(array[i]);
-	}
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
