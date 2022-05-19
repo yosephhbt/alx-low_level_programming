c589a96f6979a3ac867f5a5389694c98cd7432ff
yosephhbt <josephhbt@gmail.com>
2022-05-19 11:45:35 -0700

    singly linked

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,79 +1,30 @@
+#include "lists.h"
 #include <stdio.h>
-#include <lists.h>
-#include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- * _strlen - function to get length of a string
- * @s: string
- * Return: length of array of characters
- */
-int _strlen(char *s)
-{
-	int i;
-
-	
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-/**
- * _strdup - returns a pointer to a newly allocated space in memory
- * @str: string to copy
- * Return: Pointer
- */
-char *_strdup(const char *str)
-{
-	int l = 0, i;
-	char *s;
-
-
-	if (str == NULL)
-		return (0);
-	while (*(str + l))
-		l++;
-	s = malloc(sizeof(char) * l + 1);
-	if (s == 0)
-		return (0);
-	for (i = 0; i <= l; i++)
-		*(s + i) = *(str + i);
-	return (s);
-}
-/**
- * add_node - adds a new node at the beginning
- * @head: pointer to head of singly linked list
- * @str: string to add as new node in list
- * Return: the address of new element
+ * add_node - adds a node at the beginning of list
+ * @head: points to the head of list
+ * @str: what goes in the new node
+ * Return: address of new node or NULL
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *tmp;
-	list_t *new_node = malloc(sizeof(list_t));
-
-
-	if (new_node == 0)
+	int i;
+	list_t *new;
+	
+	
+	if (!(head && str))
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (!new)									return (NULL);
+	for (i = 0; str[i]; i++)							{}
+	new->str = strdup(str);
+	if (!(new->str))
 	{
-		return (0);
+		free(new);									return (NULL);
 	}
-	if (str == 0)
-	{
-		new_node->str = 0;
-		new_node->len = 0;
-	}
-	else
-	{
-		tmp = _strdup(str);
-		if (tmp == 0)
-		{
-			free(new_node);
-			return (0);
-		}
-		new_node->str = tmp;
-		new_node->len = _strlen(tmp);
-	}
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	new->len = i;									new->next = *head;
+	*head = new;
+	return (new);
 }
