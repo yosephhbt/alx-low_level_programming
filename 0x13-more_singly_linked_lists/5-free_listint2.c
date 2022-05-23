4d8304ced3ee4cbd2ece3b24a9980267083bd794
yosephhbt <josephhbt@gmail.com>
2022-05-23 10:56:00 -0700

    more single linked

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - frees memory of a list
+ * @head: pointer to head of singly linked list
+ * Return: no return
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+
+	if (head)
+	{
+		while (*head != 0)
+		{
+			tmp = *head;
+			*head = tmp->next;
+			free(tmp);
+		}
+	}
+}
