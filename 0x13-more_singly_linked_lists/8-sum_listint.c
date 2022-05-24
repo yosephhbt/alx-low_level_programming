3832d08212148cdc757a9f29223462635a5458b9
yosephhbt <josephhbt@gmail.com>
2022-05-24 10:45:40 -0700

    more singly linked

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all the data
+ * @head: head pointer of singly linked list
+ * Return: sum of all the data
+ */
+int sum_listint(listint_t *head)
+{
+	int add = 0;
+
+
+	if (head == 0)
+		return (0);
+	while (head != 0)
+	{
+		add += head->n;
+		head = head->next;
+	}
+	return (add);
+}
