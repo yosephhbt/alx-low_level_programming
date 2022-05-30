afaf9ec713fd462f7a1e6dcf0f0a29b7ede6232f
yosephhbt <josephhbt@gmail.com>
2022-05-30 06:28:35 -0700

    file io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, checkw, l = 0;
+
+
+	if (filename == 0)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[l] != 0)
+			l++;
+		checkw = write(fd, text_content, l);
+		if (checkw == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
